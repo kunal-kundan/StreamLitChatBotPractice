91672d15502f1aae00dab458f3b3ead396496f1e
kunal-kundan <kunalmv043@gmail.com>
2025-12-25 20:47:56 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -12,7 +12,7 @@ st.set_page_config(
     page_icon="🤖",
     layout="centered",
 )
-st.title("💬Kundan Ki Kalam se: Up for a Samvaad ?")
+st.title("💬Kundan Ki Kalam se: Up for an AI flavored Samvaad ?")
 
 # initiate chat history
 if "chat_history" not in st.session_state:
